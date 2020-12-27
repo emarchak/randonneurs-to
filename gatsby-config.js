f8ec27571d93b7f47522727d26f73aa45970e6b1
emarchak <erin@emarchak.com>
2020-12-27 15:47:28 -0500

    Add google sheets as data point.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 /* eslint-disable no-undef */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   flags: { DEV_SSR: true },
   siteMetadata: {
@@ -38,6 +42,24 @@ module.exports = {
             "Dawning of a New Day",
             "Roboto:400,400i,700",
           ],
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-google-spreadsheets',
+      options: {
+        spreadsheetId: '1KUxsyTLQERX43VAiKMR_LKvIpwzGeoOEcHuRKUvmjOM',
+        credentials: {
+          type: 'service_account',
+          project_id: 'randonneurs-to',
+          private_key_id: process.env.GS_PRIVATE_KEY_ID,
+          private_key: process.env.GS_PRIVATE_KEY.replace(/(\\r)|(\\n)/g, '\n'),
+          client_email: 'randonneurstosheets@randonneurs-to.iam.gserviceaccount.com',
+          client_id: '116465140767443102500',
+          auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+          token_uri: 'https://oauth2.googleapis.com/token',
+          auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+          client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/randonneurs-to%40appspot.gserviceaccount.com`,
         },
       },
     },
