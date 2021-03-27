a9857a79b9f2b38f4b9ea17e252d36f88d716175
emarchak <erin@emarchak.com>
2021-03-27 13:19:49 -0400

    Try enabling bugsnag again.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,15 +2,15 @@ const React = require('react')
 const Bugsnag = require('@bugsnag/js').default
 const BugsnagPluginReact = require('@bugsnag/plugin-react').default
 
-if (process.env.NODE_ENV === 'production') {
-//     Bugsnag.start({
-//         apiKey: process.env.BUGSNAG_API_KEY,
-//         plugins: [new BugsnagPluginReact()],
-//     })
+if (process.env.NODE_ENV !== 'development') {
+    Bugsnag.start({
+        apiKey: process.env.GATSBY_BUGSNAG_API_KEY,
+        plugins: [new BugsnagPluginReact()],
+    })
 
-//     const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
+    const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
 
-//     exports.wrapRootElement = ({ element }) => (
-//         <ErrorBoundary>{element}</ErrorBoundary>
-//     )
+    exports.wrapRootElement = ({ element }) => (
+        <ErrorBoundary>{element}</ErrorBoundary>
+    )
 }
