3dd3aa063412d8d6f805e5cc3bc0f5357cf19a74
emarchak <erin@emarchak.com>
2021-04-02 14:36:49 -0400

    Set registration deadline to friday at 6pm.

--- src/components/registration/hooks/useAllowedStartTimes.test.ts
+++ src/components/registration/hooks/useAllowedStartTimes.test.ts
@@ -2,41 +2,51 @@ import { advanceTo, clear } from 'jest-date-mock'
 import { useAllowedStartTimes, addDays } from "./useAllowedStartTimes"
 
 describe('useAllowedStartTimes', () => {
-    beforeEach(() => {
-        advanceTo(new Date('August 18 2019'))
-    })
-
     afterEach(() => {
         clear()
     })
 
-    it('requires the start date to be a day in the future for unscheduled events', () => {
-        const { allowedStartTimes, } = useAllowedStartTimes()
+    describe('allowedStartTimes()', () => {
+        it('requires the start date to be a day in the future for unscheduled events', () => {
+            advanceTo(new Date('August 18 2021'))
+            const { allowedStartTimes } = useAllowedStartTimes()
 
-        expect(allowedStartTimes(addDays(0))).toBeFalsy()
-        expect(allowedStartTimes(addDays(1))).toBeTruthy()
+            expect(allowedStartTimes(new Date('August 18 2021'))).toBeFalsy()
+            expect(allowedStartTimes(new Date('August 19 2021'))).toBeTruthy()
+        })
+
+        it('requires start date to be on scheduled date', () => {
+            advanceTo(new Date('August 1 2021'))
+
+            const { allowedStartTimes } = useAllowedStartTimes()
+
+            const scheduledDate = new Date('August 20 2021')
+            expect(allowedStartTimes(new Date('August 17 2021'), scheduledDate)).toBeFalsy()
+            expect(allowedStartTimes(new Date('August 19 2021'), scheduledDate)).toBeFalsy()
+            expect(allowedStartTimes(new Date('August 26 2021'), scheduledDate)).toBeFalsy()
+            expect(allowedStartTimes(new Date('August 20 2021'), scheduledDate)).toBeTruthy()
+        })
     })
 
-    it('requires start date to be on scheduled date', () => {
-        const { allowedStartTimes } = useAllowedStartTimes()
-        const notAllowed = addDays(2)
+    describe('allowedToRegister()', () => {
+        it('allows riders to register before Friday at 6pm ET before scheduled date', () => {
+            const { allowedToRegister } = useAllowedStartTimes()
 
-        expect(allowedStartTimes(addDays(2), notAllowed)).toBeFalsy()
-        expect(allowedStartTimes(addDays(7), notAllowed)).toBeFalsy()
+            advanceTo(new Date('Fri April 2 2021 17:59:30 EDT'))
+            const rideOnSaturday = new Date('Sat April 3 2021 09:20:00 EDT')
+            expect(allowedToRegister(rideOnSaturday)).toBeTruthy()
 
-        const allowed = addDays(8)
-        expect(allowedStartTimes(addDays(7), allowed)).toBeFalsy()
-        expect(allowedStartTimes(addDays(9), allowed)).toBeFalsy()
-        expect(allowedStartTimes(addDays(8), allowed)).toBeTruthy()
-    })
+            advanceTo(new Date('Wed April 7 2021 09:59:30 EDT'))
+            const rideNextSaturday = new Date('Sat April 10 2021 09:20:00 EDT')
+            expect(allowedToRegister(rideNextSaturday)).toBeTruthy()
+        })
 
-    it('allows riders to register up to three days before scheduled date', () => {
-        const { allowedToRegister } = useAllowedStartTimes()
+        it('stops riders from registering after Friday at 6pm ET before scheduled date', () => {
+            const { allowedToRegister } = useAllowedStartTimes()
 
-        const allowed = addDays(8)
-        expect(allowedToRegister(allowed)).toBeTruthy()
-
-        const notAllowed = addDays(2)
-        expect(allowedToRegister(notAllowed)).toBeFalsy()
+            advanceTo(new Date('Fri April 2 2021 18:01:30 EDT'))
+            const rideOnSaturday = new Date('Sat April 3 2021 09:20:00 EDT')
+            expect(allowedToRegister(rideOnSaturday)).toBeFalsy()
+        })
     })
 })
--- src/components/registration/hooks/useAllowedStartTimes.ts
+++ src/components/registration/hooks/useAllowedStartTimes.ts
@@ -1,18 +1,33 @@
-const today = new Date(Date.now())
-const inFuture = (date: Date, after = today) => new Date(date).setHours(0) > new Date(after).setHours(23)
-const onDate = (date: Date, now = today) => new Date(date).setHours(0) === new Date(now).setHours(0)
-export const addDays = (number: number) => new Date(new Date(today).setDate(today.getDate() + number))
+
+const inFutureDate = (date: Date, after: Date) => new Date(date).setHours(0) > new Date(after).setHours(23)
+const onDate = (date: Date, now: Date) => new Date(date).setHours(0) === new Date(now).setHours(0)
+
+export const addDays = (date: Date, number: number) => new Date(new Date(date).setDate(date.getDate() + number))
+
+function getFridayBefore(date: Date) {
+    const friday = 5
+    const weekday = date.getDay()
+    const diff = (weekday < friday) ? (7 - friday + weekday) : (friday - weekday)
+
+    return addDays(date, diff)
+}
 
 export const useAllowedStartTimes = () => {
     const allowedStartTimes = (time: Date, scheduleTime?: Date) => {
+        const today = new Date(Date.now())
         if (scheduleTime) {
-            return onDate(time, scheduleTime) && inFuture(time, addDays(6))
+            return onDate(time, scheduleTime) && inFutureDate(time, addDays(today, 6))
         }
-        return inFuture(time, today)
+        return inFutureDate(time, today)
     }
 
     const allowedToRegister = (scheduleTime: Date) => {
-        return inFuture(scheduleTime, addDays(2))
+        const now = new Date(Date.now())
+        const registrationDeadline = getFridayBefore(scheduleTime)
+        registrationDeadline.setHours(18)
+        registrationDeadline.setMinutes(0)
+
+        return now < registrationDeadline
     }
     return { allowedToRegister, allowedStartTimes }
 }
