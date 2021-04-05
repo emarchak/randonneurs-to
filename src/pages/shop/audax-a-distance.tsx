8681d668ef6b9fd7ebf1017e9caca544b6f61538
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-04-04 22:10:38 -0400

    Correct spelling.

--- src/pages/shop/audax-a-distance.tsx
+++ src/pages/shop/audax-a-distance.tsx
@@ -17,14 +17,13 @@ const AudaxShopPage = () => {
                 description="Purchase patches for Club audax à distance. We are riders that are commited to socially distancing ourself, to do
             the right thing. If we must be alone in these tough times, let us be alone together."
             />
-
             <ContentWrapper>
                 <TabMenu tabs={lonelinessRoutes} activeRoute='/shop/audax-a-distance/' />
-                <h1>Club Audax à Distance</h1><h2>cycling patch</h2>
             </ContentWrapper>
             <ContentWrapper container>
                 <ContentChild>
-                    <p>A custom patch to celebrate riding by your lonesome! <em>Club Audax à Distance</em> is a play on "long distance relationships" and the time we spend together. Get your own randonneuring cycling patch to put on bags, jerseys, tool poutches, or put in your map pouch!</p>
+                    <h1>Club Audax à Distance</h1><h2>cycling patch</h2>
+                    <p>A custom patch to celebrate riding by your lonesome! <em>Club Audax à Distance</em> is a play on "long distance relationships" and the time we spend together. Get your own randonneuring cycling patch to put on bags, jerseys, tool pouches, or put in your map pouch!</p>
 
                     <p>Patches are 1.5" x 4.5", and have an iron-on backing.</p>
 
