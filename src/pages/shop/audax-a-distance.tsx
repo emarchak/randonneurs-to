ab4700b8cd715e4413bcceee94085d3204de97d0
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-04-04 21:45:06 -0400

    Add audax tabs.

--- src/pages/shop/audax-a-distance.tsx
+++ src/pages/shop/audax-a-distance.tsx
@@ -4,6 +4,8 @@ import { Layout } from 'src/components/layout'
 import { SEO } from 'src/components/seo'
 import { ContentWrapper, ContentChild } from "src/components/content-wrapper"
 import Loadable from "@loadable/component"
+import { TabMenu } from 'src/components/tabmenu'
+import { lonelinessRoutes } from '../loneliness'
 
 const BuyButton = Loadable(() => import('../../components/buybutton'))
 
@@ -15,10 +17,13 @@ const AudaxShopPage = () => {
                 description="Purchase patches for Club audax à distance. We are riders that are commited to socially distancing ourself, to do
             the right thing. If we must be alone in these tough times, let us be alone together."
             />
+
+            <ContentWrapper>
+                <TabMenu tabs={lonelinessRoutes} activeRoute='/shop/audax-a-distance/' />
+                <h1>Club Audax à Distance</h1><h2>cycling patch</h2>
+            </ContentWrapper>
             <ContentWrapper container>
                 <ContentChild>
-                    <h1>Club Audax à Distance</h1><h2>cycling patch</h2>
-
                     <p>A custom patch to celebrate riding by your lonesome! <em>Club Audax à Distance</em> is a play on "long distance relationships" and the time we spend together. Get your own randonneuring cycling patch to put on bags, jerseys, tool poutches, or put in your map pouch!</p>
 
                     <p>Patches are 1.5" x 4.5", and have an iron-on backing.</p>
