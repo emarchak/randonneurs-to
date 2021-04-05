f66fd2b7c2227f2b394d7012ed74e2f4db72c57b
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-04-04 21:31:47 -0400

    Style tweaks (#51)
    * style tweaks.
    
    * Add a bit more styles.
    
    * More button styles.
    
    * Extract out button variations.
    
    * add border to menu.

--- src/components/form/buttons.tsx
+++ src/components/form/buttons.tsx
@@ -1,16 +1,33 @@
+import { Link } from 'gatsby'
 import React from 'react'
 import styles from "../styles/form.module.scss"
 
-type ButtonProps = {
+type ButtonTypes = {
+    primary?: boolean,
+    secondary?: boolean,
+    small?: boolean,
+    block?: boolean,
+}
+
+const getButtonClassName = ({ primary = false, secondary = false, block = false, small = false }) => (
+    [
+        styles.button,
+        ...(primary ? [styles.primaryButton] : []),
+        ...(secondary ? [styles.secondaryButton] : []),
+        ...(block ? [styles.blockButton] : []),
+        ...(small ? [styles.smallButton] : []),
+    ].join(' '))
+
+type ButtonProps = ButtonTypes & {
     handleClick: (evt: any) => any,
     disabled?: boolean,
     className?: string,
     children: React.ReactChild
 }
 
-export const Button = ({ handleClick, disabled, className, children }: ButtonProps) => (
+export const Button = ({ handleClick, disabled, className, children, ...props }: ButtonProps) => (
     <button
-        className={className + ' ' + styles.button}
+        className={className + ' ' + getButtonClassName(props)}
         disabled={disabled}
         onClick={handleClick}
     >
@@ -27,10 +44,19 @@ type SubmitButtonProps = {
 export const SubmitButton = ({ handleSubmit, disabled = false, children }: SubmitButtonProps) => (
     <button
         type="submit"
-        className={styles.submit + ' ' + styles.button}
+        className={getButtonClassName({ primary: true })}
         disabled={disabled}
         onClick={handleSubmit}
     >
         {children}
     </button>
+)
+
+type LinkButtonProps = ButtonTypes & {
+    to: string,
+    children: React.ReactChild,
+}
+
+export const LinkButton = ({ to, children, ...props }: LinkButtonProps) => (
+    <Link to={to} className={getButtonClassName(props)}>{children}</Link>
 )
