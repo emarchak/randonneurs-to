258beb6849519e1b711c8688e638f26aff46cf87
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-04-17 15:37:41 -0400

    Add to gatsby-browser.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,21 @@
 const React = require('react')
 const Bugsnag = require('@bugsnag/js').default
 const BugsnagPluginReact = require('@bugsnag/plugin-react').default
+const BuyButton = require('./src/components/buybutton')
+
 
 if (process.env.NODE_ENV !== 'development') {
     Bugsnag.start({
         apiKey: process.env.GATSBY_BUGSNAG_API_KEY,
         plugins: [new BugsnagPluginReact()],
     })
-
-    const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
-
-    exports.wrapRootElement = ({ element }) => (
-        <ErrorBoundary>{element}</ErrorBoundary>
-    )
 }
+const ErrorBoundary = process.env.NODE_ENV === 'development' ? React.Fragment : Bugsnag.getPlugin('react').createErrorBoundary(React)
+
+exports.wrapRootElement = ({ element }) =>  (
+    <ErrorBoundary>
+        <BuyButton.BuyButtonProvider>
+            {element}
+        </BuyButton.BuyButtonProvider>
+    </ErrorBoundary>
+)
