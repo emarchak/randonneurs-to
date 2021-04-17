827910d501fae4a6dcef637774c3c873ee473f13
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-04-17 18:45:54 -0400

    Fix tests.

--- src/components/registration/components/registration-form-brevet.test.tsx
+++ src/components/registration/components/registration-form-brevet.test.tsx
@@ -19,7 +19,7 @@ jest.mock('src/hooks/useBrevets', () => ({
                 chapter: 'Toronto',
                 event: 'populaire',
                 distance: '60',
-                date: new Date('Sat April 10 2021 09:20:00 EDT'),
+                date: new Date('Sat August 7 2021 09:20:00 EDT'),
                 route: 'Rouge Ramble 60',
                 startLocation: 'Second Cup, 355 Danforth Ave, Toronto',
                 id: 1,
@@ -44,7 +44,7 @@ jest.mock('src/hooks/useCheckRiderMembership', () => ({
 
 describe('<RegistrationForm>', () => {
     beforeEach(() => {
-        advanceTo(new Date('Thu April 1 2021 09:00:00 EDT'))
+        advanceTo(new Date('Wed August 4 2021 09:00:00 EDT'))
     })
 
     afterEach(() => {
@@ -110,7 +110,7 @@ describe('<RegistrationForm>', () => {
     it('records the registration when submitted', async () => {
         const fetchSpy = jest.spyOn(isomorphicUnfetch, 'default')
         const mount = render(<RegistrationFormBrevet />)
-        const rideDate = new Date('Sat April 10 2021 09:20:00 EDT')
+        const rideDate = new Date('Sat August 7 2021 09:20:00 EDT')
 
         fireEvent.change(mount.getByLabelText(/name/i), {
             target: { value: 'Foo Bar' },
@@ -191,7 +191,7 @@ describe('<RegistrationForm>', () => {
     })
 
     it('disables registrations for events that are not allowedToRegister', () => {
-        advanceTo(new Date('Fri April 10 2021 17:20:00 EDT'))
+        advanceTo(new Date('Fri August 7 2021 17:20:00 EDT'))
 
         const mount = render(<RegistrationFormBrevet />)
 
