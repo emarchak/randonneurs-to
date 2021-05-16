40ccb3f0aa84f19ab8c603156efd598d46a49b01
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-05-16 16:33:45 -0400

    Create Form component.

---
+++ src/components/form/components.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+import { form, formWrapper } from "../styles/form.module.scss"
+
+type FormProps = {
+    name: string,
+    children: React.ReactNode
+    className?: string
+}
+
+export const Form = ({ name, children, className }: FormProps) => (
+    <div className={formWrapper}>
+        <form
+            name={name}
+            method='post'
+            data-netlify='true'
+            data-netlify-honeypot='bot-field'
+            className={[form, className].join(' ')}
+        >
+            {children}
+        </form>
+    </div>
+)
