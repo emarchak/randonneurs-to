84177a8d0fe6ae211fafa361fa27ad4480ca9969
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-06-13 12:24:10 -0400

    Add text.

--- src/pages/registration/covid.tsx
+++ src/pages/registration/covid.tsx
@@ -3,15 +3,20 @@ import { Layout } from 'src/components/layout'
 import { SEO } from 'src/components/seo'
 import { ContentChild, ContentWrapper } from "src/components/content-wrapper"
 import { CovidForm } from 'src/components/registration/components/CovidForm'
+import { Link } from 'src/components/link'
 
 const CovidScreening = () => {
     return (
         <Layout hideHeader>
             <SEO title='COVID screening form' />
             <ContentWrapper>
-                <h1>COVID screening form</h1><h2>of Randonneurs Ontario</h2>
+                <h1>COVID screening form</h1><h2>for Randonneurs Ontario</h2>
+                <p>All riders must complete self-screen before starting a ride. A missing screening will result in a DNF.</p>
             </ContentWrapper>
             <CovidForm formUrl="/registration/covid" />
+            <ContentWrapper>
+                <p>Screening questionnaire based off of the <Link href="https://www.toronto.ca/wp-content/uploads/2020/05/95ea-Screening-poster-retail-entrance-TPH.pdf">Toronto Public Health screening</Link>.</p>
+            </ContentWrapper>
         </Layout >
     )
 }
