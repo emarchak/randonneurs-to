d90d2d80a65afde5b267fa008a8140df22a4b62a
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-06-13 13:56:23 -0400

    Extract out formatMessage.

--- src/components/form/utils/formatSlackMessage.ts
+++ src/components/form/utils/formatSlackMessage.ts
@@ -1,22 +1,12 @@
-import fetch from 'isomorphic-unfetch'
-import { getDateTimeShort } from 'src/utils'
+import { formatMessage } from './formatMessage'
 import { FieldLabel, FormData } from './types'
 
 type formatSlackMessageArgs = { formData: FormData, fieldLabels: FieldLabel }
     & ({ formName?: never, message: string } | { formName: string, message?: never })
 
-const sentenceCase = (str: string) => {
-    const result = str.replace(/([A-Z])/g, " $1")
-    return result.charAt(0).toUpperCase() + result.slice(1)
-}
-
 export const formatSlackMessage = ({ formName, formData, fieldLabels, message }: formatSlackMessageArgs) => ({
     message: message || `Submission from ${formName}`,
     attachments: [
-        Object.keys(formData).map(key => {
-            const value = formData[key] instanceof Date ? getDateTimeShort(formData[key]) : formData[key]
-            const label = fieldLabels[key] || sentenceCase(key)
-            return `${label}: ${value}`
-        }).join(' \n ')
+        formatMessage({ formData, fieldLabels })
     ]
 })
--- src/components/form/utils/index.ts
+++ src/components/form/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './formSubmit'
 export * from './formatSlackMessage'
+export * from './formatMessage'
 export * from './types'
 export * from './validate'
