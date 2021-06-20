ed13dc44095211a3937a381b814c63c57d7db46d
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-06-20 13:51:49 -0400

    Encode url params nicely.

--- functions/brevet-card/brevet-card.js
+++ functions/brevet-card/brevet-card.js
@@ -1,26 +1,40 @@
-require('isomorphic-unfetch')
-const {FormData} = require('formdata-node')
-
 const cardEndpoint = 'https://www.randonneursontario.ca/brevetcard/cardtopdf.php'
+
+const formEncode = data => Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(
+        typeof data[key] === 'string' ? data[key] : JSON.stringify(data[key])
+          ))
+      .join("&")
 
 const handler = async (event) => {
   try {
     const riderName = event.queryStringParameters.name || ''
     const startTime = event.queryStringParameters.startTime || new Date()
 
-    const body = new FormData()
-    body.set('distance',    '300')
-    body.set('evname',      'Kissing Bridge 300')
-    body.set('evstart',     'June 19 2021')
-    body.set('maxhours',    '20')
-    body.set('maxminutes',  '00')
-    body.set('emergetel',   'vp@randonneurs.to')
-    body.set('riderlist',   `[{"fname":"${riderName}"}]`);
-    body.set('controllist', `[{  "dist":"0.0 km",  "name":"Start",  "open":"O: Sat 04h59",  "close":"C: Sat 05h59"}]`);
+    const body = {
+      distance:    "300",
+      evname:      "Kissing Bridge 300",
+      evstart:     "June 19 2021",
+      maxhours:    "20",
+      maxminutes:  "00",
+      emergetel:   "vp@randonneurs.to",
+      riderlist:   [{fname: riderName}],
+      controllist: [    
+        {       
+          dist: "0.0 km",
+          name: "Start",
+          open: "O: Sat 04h59",
+          close: "C: Sat 05h59"    
+        },
+      ],
+    }
 
     const response = await fetch(cardEndpoint, {
       method: 'POST',
-      body,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formEncode(body)
     })
 
     if (!response.ok) {
@@ -32,7 +46,7 @@ const handler = async (event) => {
     return { 
       statusCode: response.status,
       headers: {
-        'Content-Disposition': `filename=brevetcard-${riderName}-${startTime.toString()}.pdf`,
+        'Content-Disposition': response.headers.get('content-disposition'),
       },
       body: Buffer.from(memBuffer).toString('base64'),
       isBase64Encoded: true,
