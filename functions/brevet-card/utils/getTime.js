d32ad8081d3da1b4e12243902c5ff181106978bf
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-06-27 13:13:29 -0400

    Calculate controls correctly.

--- functions/brevet-card/utils/getTime.js
+++ functions/brevet-card/utils/getTime.js
@@ -1,36 +1,73 @@
 // Time limits and calculations from 
 // https://rusa.org/pages/acp-brevet-control-times-calculator
-const addTime = (d, {h, m}) => new Date(d).setTime(d.getTime() + (h*60*60*1000) + (m*60*1000));
+
+const addTime = (d, {h = 0, m = 0}) => {
+  const newTime = new Date(d);
+  newTime.setTime(d.getTime() + (h*60*60*1000) + (m*60*1000))
+  return newTime
+};
 
 const brevetTimeLimits = {
-    200:  {h:13,  m: 30},
-    300:  {h:20,  m: 0},
-    400:  {h:27,  m: 0},
-    600:  {h:40,  m: 0},
-    1000: {h:75,  m: 0},
-    1200: {h:90,  m: 0},
-    1400: {h:116, m: 40},
-    2200: {h:220, m: 0}
+  200:  {h:13,  m: 30},
+  300:  {h:20,  m: 0},
+  400:  {h:27,  m: 0},
+  600:  {h:40,  m: 0},
+  1000: {h:75,  m: 0},
+  1200: {h:90,  m: 0},
+  1400: {h:116, m: 40},
+  2200: {h:220, m: 0}
 }
 
 const getTimeLimit = (distance) => brevetTimeLimits[distance]
 
-const getOpenTime = ({startTime, controlDistance, eventDistance}) => {
-    if (controlDistance < 1) {
-        return startTime
-    }
-
-    return startTime
+const getOpenTime = ({startTime, controlDistance}) => {
+  switch (true) {
+    case (controlDistance < 1):
+      return startTime
+    case (controlDistance < 200):
+      return addTime(startTime, {
+        h: controlDistance / 34
+      })
+    case (controlDistance >= 200 && controlDistance < 400):
+      return addTime(startTime, {
+        h: controlDistance / 32
+      })
+    case (controlDistance >= 400 && controlDistance < 600):
+      return addTime(startTime, {
+        h: controlDistance / 30
+      })
+    case (controlDistance >= 600 && controlDistance < 1000):
+      return addTime(startTime, {
+        h: controlDistance / 28
+      })
+    case (controlDistance >= 1000):
+      return addTime(startTime, {
+        h: controlDistance / 26
+      })
+  }
 }
 
 const getCloseTime = ({startTime, controlDistance, eventDistance}) => {
-    
-    if (controlDistance > eventDistance) {
-        const timeLimit = brevetTimeLimits[eventDistance]
-        return addTime(startTime, timeLimit)
-    }
-    return startTime
+  switch (true) {
+    case (controlDistance > eventDistance):
+      return addTime(startTime, brevetTimeLimits[eventDistance])
+    case (controlDistance <= 60):
+      return addTime(startTime, {
+        h: 1 + (controlDistance / 20)
+      })
+      case (controlDistance < 600):
+      return addTime(startTime, {
+        h: controlDistance / 15
+      })
+    case (controlDistance >= 600 && controlDistance < 1000):
+      return addTime(startTime, {
+        h: controlDistance / 11.428
+      })
+    case (controlDistance >= 1000):
+      return addTime(startTime, {
+        h: controlDistance / 13.3333
+      })
+  }
 }
-
 
 module.exports = { getTimeLimit, getOpenTime, getCloseTime }
--- functions/brevet-card/utils/getTime.test.js
+++ functions/brevet-card/utils/getTime.test.js
@@ -1,7 +1,7 @@
 const { getTimeLimit, getOpenTime, getCloseTime } = require('./getTime')
 
 
-const startTime = new Date('June 6 06:00')
+const startTime = new Date('June 6 2021 06:00')
 const eventDistance = 1300
 const args = {startTime, eventDistance}
 
@@ -16,38 +16,49 @@ describe('getTimeLimit()', () => {
 
 describe('getCloseTime()', () => {
     it('uses 15km/h for controls < 600 from the start', () => {
-        expect(getCloseTime({...args, controlDistance: 98})).toBe('')
+        const closeTime = getCloseTime({...args, controlDistance: 98})
+        expect(closeTime.toString()).toMatch(/Jun 06 2021 12:32/)
     })
-
     it('uses 11.428km/h for controls 600 - 1000 from the start', () => {
-        expect(getCloseTime({...args, controlDistance: 650})).toBe('')
+        const closeTime = getCloseTime({...args, controlDistance: 650})
+        expect(closeTime.toString()).toMatch(/Jun 08 2021 14:52/)
     })
-
-    it('uses 13.333km/h for controls > 1000  from the start', () => {
-        expect(getCloseTime({...args, controlDistance: 1098})).toBe('')
+    it('uses 13.333km/h for controls > 1000 from the start', () => {
+        const closeTime = getCloseTime({...args, controlDistance: 1098})
+        expect(closeTime.toString()).toMatch(/Jun 09 2021 16:21/)
     })
-
     it('uses time limit for controls past the event distance', () => {
-        expect(getCloseTime({...args, eventDistance: 200, controlDistance: 201})).toBe('')
+        const closeTime = getCloseTime({...args, eventDistance: 200, controlDistance: 201})
+        expect(closeTime.toString()).toMatch(/Jun 06 2021 19:30/)
     })
+    it('uses French variation for controls < 60km from the start', () => {
+      const closeTime = getCloseTime({...args, controlDistance: 58})
+      expect(closeTime.toString()).toMatch(/Jun 06 2021 09:54/)
+  })
 })
 describe('getOpenTime()', () => {
+    it('uses start time for controls < 1 from start', ()=> {
+        const openTime = getOpenTime({...args, controlDistance: .7})
+        expect(openTime.toString()).toMatch(/Jun 06 2021 06:00/)
+    })
     it('uses 34km/h for controls < 200 from the start', () => {
-        expect(getOpenTime({...args, controlDistance: 98})).toBe('')
+        const openTime = getOpenTime({...args, controlDistance: 199})
+        expect(openTime.toString()).toMatch(/Jun 06 2021 11:51/)
     })
-
     it('uses 32km/h for controls 200 - 400 from the start', () => {
-        expect(getOpenTime({...args, controlDistance: 298})).toBe('')
+        const openTime = getOpenTime({...args, controlDistance: 201})
+        expect(openTime.toString()).toMatch(/Jun 06 2021 12:16/)
     })
-
     it('uses 30km/h for controls 400-600 from the start', () => {
-        expect(getOpenTime({...args, controlDistance: 598})).toBe('')
+        const openTime = getOpenTime({...args, controlDistance: 501})
+        expect(openTime.toString()).toMatch(/Jun 06 2021 22:42/)
     })
-
     it('uses 28km/h for controls 600-1000 from the start', () => {
-        expect(getOpenTime({...args, controlDistance: 978})).toBe('')
+        const openTime = getOpenTime({...args, controlDistance: 701})
+        expect(openTime.toString()).toMatch(/Jun 07 2021 07:02/)
     })
     it('uses 26km/h for controls > 1000 from the start', () => {
-        expect(getOpenTime({...args, controlDistance: 1098})).toBe('')
+        const openTime = getOpenTime({...args, controlDistance: 1001})
+        expect(openTime.toString()).toMatch(/Jun 07 2021 20:30/)
     })
 })
