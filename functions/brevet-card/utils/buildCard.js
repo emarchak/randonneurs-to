6697777b02b160ad1a23eb2b03eed91c07d841b3
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-07-13 19:40:50 -0400

    Feat: better card logging. (#118)

--- functions/brevet-card/utils/buildCard.js
+++ functions/brevet-card/utils/buildCard.js
@@ -9,20 +9,23 @@ const localeOpts = {
 }
 
 const buildCard = async ({riderName, customStartTime, scheduleId}) =>  {
-    const event = await getEvent(scheduleId)
-    const timeLimit = getTimeLimit(event.distance)
-    const controls = await getControls({event, customStartTime})
+  const event = await getEvent(scheduleId)
+  const timeLimit = getTimeLimit(event.distance)
+  const controls = await getControls({event, customStartTime})
 
-    return {
-        distance:    event.distance,
-        evname:      event.name,
-        evstart:     getDate(event.date),
-        maxhours:    timeLimit.h.toLocaleString('en-US', localeOpts),
-        maxminutes:  timeLimit.m.toLocaleString('en-US', localeOpts),
-        emergetel:   "vp@randonneurs.to",
-        riderlist:   [{fname: riderName}],
-        controllist: controls,
-      }
+  const card = {
+    distance:    event.distance,
+    evname:      event.name,
+    evstart:     getDate(event.date),
+    maxhours:    timeLimit.h.toLocaleString('en-US', localeOpts),
+    maxminutes:  timeLimit.m.toLocaleString('en-US', localeOpts),
+    emergetel:   "vp@randonneurs.to",
+    riderlist:   [{fname: riderName}],
+    controllist: controls,
+  }
+
+  console.log(card)
+  return card
 }
 
 module.exports = { buildCard }
