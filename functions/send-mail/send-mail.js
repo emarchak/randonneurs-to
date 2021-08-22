7d570ee81ad80a7e9230cbd3cae2c1ca62030f77
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-08-22 15:26:01 -0400

    Scaffold singlesend.

--- functions/send-mail/send-mail.js
+++ functions/send-mail/send-mail.js
@@ -1,4 +1,5 @@
 const send = require('./send')
+const singlesend = require('./singlesend')
 
 const handler = async (event) => {
   const { path, httpMethod } = event
@@ -6,7 +7,9 @@ const handler = async (event) => {
   switch (`${path}:${httpMethod}`) {
     case '/.netlify/functions/send-mail/send:POST':
       return send(event)
-    default: 
+      case '/.netlify/functions/send-mail/singlesend:GET':
+        return singlesend.get(event)
+    default:
       return {
         statusCode: 404
       }
---
+++ functions/send-mail/singlesend.js
@@ -0,0 +1,5 @@
+const getSingleSend = () => {}
+
+module.exports = {
+  get: getSingleSend
+}
