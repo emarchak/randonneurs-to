4e7808d4712758e1746731d5bfb80b03bb00ecc9
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-09-05 14:45:14 -0400

    Fix tests.

--- functions/send-mail/methods/singlesend.test.ts
+++ functions/send-mail/methods/singlesend.test.ts
@@ -3,9 +3,8 @@ import { getSendsByCategory } from "./singlesend"
 
 describe('getSendsByCategory()', () => {
   it('fetches singlesends by category', async () => {
-    const response = await getSendsByCategory({
-      body: JSON.stringify({category: 'category1'})
-    } as HandlerEvent)
+    const event: HandlerEvent = {queryStringParameters: {category: 'category1'}} as any
+    const response = await getSendsByCategory(event)
 
     const data = JSON.parse(response.body)
 
