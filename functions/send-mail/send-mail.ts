3c4a992db91221d4d2c6dbd19dde30a62b0f7cc5
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-09-05 16:06:07 -0400

    Remove unused function.

--- functions/send-mail/send-mail.ts
+++ functions/send-mail/send-mail.ts
@@ -1,6 +1,5 @@
 import { Handler } from '@netlify/functions'
 import send from './methods/send'
-import { getSendsByCategory } from './methods/singlesend'
 
 const handler: Handler = async (event) => {
   const { path, httpMethod } = event
@@ -8,8 +7,6 @@ const handler: Handler = async (event) => {
   switch (`${path}:${httpMethod}`) {
     case '/.netlify/functions/send-mail/send:POST':
       return send(event)
-    case '/.netlify/functions/send-mail/singlesends:GET':
-      return getSendsByCategory(event)
     default:
       return {
         statusCode: 404,
