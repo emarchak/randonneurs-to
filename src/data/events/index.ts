9f0c989f6687352f99c86b77b8d8a4c7d327224a
Erin Marchak <823613+emarchak@users.noreply.github.com>
2021-12-26 16:17:14 -0500

    Fix gatsby type discovery.

--- src/data/events/index.ts
+++ src/data/events/index.ts
@@ -1,3 +1,3 @@
 export { useEvents } from './useEvents'
-export type { Chapter, RideType, Event as Brevet, Event } from './useEvents'
+export type { Chapter, EventType, Event as Brevet, Event } from './useEvents'
 export { mockBrevet } from './mock'
