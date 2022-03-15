2a6ce2abdf004cce77ee1b2a362c5ea2b24ef025
Erin Marchak <823613+emarchak@users.noreply.github.com>
2022-03-14 22:22:43 -0400

    fix route build.

--- src/data/routes/useRoutes.ts
+++ src/data/routes/useRoutes.ts
@@ -16,7 +16,6 @@ export const useRoutes = () => {
       db {
         routes(
           order_by: {chapter: asc, distance: asc}
-          where: {active: {_eq: true}}
         ) {
           startLocation
           name
