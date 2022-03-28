baf36f2f385d0926b5f0ed30bc43615a21dbf9ef
Erin Marchak <823613+emarchak@users.noreply.github.com>
2022-03-27 20:17:23 -0400

    Add test and rename graphql

--- functions/ccn/methods/getMemberships.ts
+++
@@ -1,6 +0,0 @@
-import { transformResponse, fetchMemberships } from './utils'
-
-export const getMemberships = async () => {
-  const response = await fetchMemberships({})
-  return response.map((data) => transformResponse(data))
-}
