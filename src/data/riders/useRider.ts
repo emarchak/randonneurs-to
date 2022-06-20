81e7670db313e7bba3ca748a5878d4eefdac33c6
Erin Marchak <823613+emarchak@users.noreply.github.com>
2022-06-20 17:31:49 -0400

    Fix missing string

--- src/data/riders/useRider.ts
+++ src/data/riders/useRider.ts
@@ -3,9 +3,9 @@ import { request } from "graphql-request"
 import getMembership from './api/getMembership.gql'
 import { Query_Root } from 'src/graphql.gql.d'
 
-const normalize = (str = '') => str.toLowerCase().replace(/\s|-|'|’|(\(.*\))/g, '')
+const normalize = (str = '') => str?.toLowerCase().replace(/\s|-|'|’|(\(.*\))/g, '')
 
-export const useRider = ({ riderName }: { riderName?: string }) => {
+export const useRider = ({ riderName = '' }: { riderName?: string }) => {
   const { data, isLoading } = useQuery(['findMembership', riderName], async (): Promise<Query_Root | null> => {
     if (!riderName) { return null }
 
@@ -18,7 +18,7 @@ export const useRider = ({ riderName }: { riderName?: string }) => {
     return query
   })
 
-  const rider = data?.memberships?.find(m => normalize(m.riderName) === normalize(riderName))
+  const rider = data?.memberships?.find(m => normalize(m?.riderName) === normalize(riderName))
 
   return ({
     data: rider,
