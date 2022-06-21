e9a972ff32ac0cd65ce0f83f4ad150bc4b1c9e27
Erin Marchak <823613+emarchak@users.noreply.github.com>
2022-06-21 16:16:31 -0400

    Add back name normalization. (#320)
    * Add back name normalization.
    
    * Returns nothing if no rider.
    
    * Remove early return like a FOOL
    
    * Fix missing string

--- src/data/riders/useRider.ts
+++ src/data/riders/useRider.ts
@@ -3,20 +3,22 @@ import { request } from "graphql-request"
 import getMembership from './api/getMembership.gql'
 import { Query_Root } from 'src/graphql.gql.d'
 
-export const useRider = ({ riderName }: { riderName?: string }) => {
+const normalize = (str = '') => str?.toLowerCase().replace(/\s|-|'|’|(\(.*\))/g, '')
+
+export const useRider = ({ riderName = '' }: { riderName?: string }) => {
   const { data, isLoading } = useQuery(['findMembership', riderName], async (): Promise<Query_Root | null> => {
-    if (!riderName) return null
+    if (!riderName) { return null }
 
     const query = await request(
       'https://randonneurs-to.hasura.app/v1/graphql',
       getMembership,
       { riderName }
     )
-    return query
 
+    return query
   })
 
-  const rider = data?.memberships?.find(m => m.riderName === riderName)
+  const rider = data?.memberships?.find(m => normalize(m?.riderName) === normalize(riderName))
 
   return ({
     data: rider,
