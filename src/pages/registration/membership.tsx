b137a0ad8defba78f0fa4fafc06dc1a11060c062
Erin Marchak <823613+emarchak@users.noreply.github.com>
2022-06-29 10:43:51 -0400

    Update membership.tsx

--- src/pages/registration/membership.tsx
+++ src/pages/registration/membership.tsx
@@ -25,19 +25,19 @@ const MemberRegistration = ({path}: PageProps) => {
                     <h3>Trial Member</h3>
                     <h4><strong>FREE</strong> + OCA Membership</h4>
                     <p><small>There is no charge for rider for their first event, no matter the distance. First time riders must have an OCA Membership through Randonneurs Ontario or another OCA affiliated club.</small></p>
-                    <LinkButton href="https://ccnbikes.com/#!/events/randonneurs-ontario-membership-2021" secondary small block>Join as a trial</LinkButton>
+                    <LinkButton href="https://ccnbikes.com/#!/events/randonneurs-ontario-membership-2022" secondary small block>Join as a trial</LinkButton>
                 </ContentChild>
                 <ContentChild>
                     <h3>Individual Membership</h3>
                     <h4><strong>$40.00 CDN</strong> + OCA Membership </h4>
                     <p><small>Full members may register for all standard rides throughout the season at no additional cost, can vote at the Annual General Meeting, and are invited to the Annual Awards dinner.</small></p>
-                    <LinkButton href="https://ccnbikes.com/#!/events/randonneurs-ontario-membership-2021" primary small block>Join as an individual</LinkButton>
+                    <LinkButton href="https://ccnbikes.com/#!/events/randonneurs-ontario-membership-2022" primary small block>Join as an individual</LinkButton>
                 </ContentChild>
                 <ContentChild>
                     <h3>Family Membership</h3>
                     <h4><strong>$40.00 CDN</strong> + OCA Membership <strong>per family member</strong></h4>
                     <p><small>The Family Membership receives all of the benefits of individual memberships, and is for a family living at the same address.</small></p>
-                    <LinkButton href="https://ccnbikes.com/#!/events/randonneurs-ontario-membership-2021" secondary small block>Join as a family</LinkButton>
+                    <LinkButton href="https://ccnbikes.com/#!/events/randonneurs-ontario-membership-2022" secondary small block>Join as a family</LinkButton>
                 </ContentChild>
             </ContentWrapper>
         </Layout >
