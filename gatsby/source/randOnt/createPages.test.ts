31837951e0ea90fe3c9d4b0ad80b038c4e3154ff
Erin Marchak <emarchak@goodwatercap.com>
2022-11-13 16:16:21 -0500

    Create calendar files.

---
+++ gatsby/source/randOnt/createPages.test.ts
@@ -0,0 +1,83 @@
+import { createPages } from './createPages'
+import * as FS from 'fs'
+import * as ICS from 'ics'
+
+
+describe('createPages', () => {
+  const existsMock = jest.spyOn(FS, 'existsSync')
+  const mkdirMock = jest.spyOn(FS, 'mkdirSync')
+  const writeFileMock = jest.spyOn(FS, 'writeFileSync')
+  const icsSpy = jest.spyOn(ICS, 'createEvents')
+  const graphql = jest.fn()
+
+  beforeEach(() => {
+    existsMock.mockImplementation(jest.fn().mockReturnValue(false))
+    mkdirMock.mockReturnValue('')
+    writeFileMock.mockImplementation(jest.fn())
+  })
+
+  afterEach(() => {
+    existsMock.mockClear()
+    mkdirMock.mockClear()
+    writeFileMock.mockClear()
+    graphql.mockClear()
+    icsSpy.mockClear()
+  })
+  it('returns null if no data is returned', async () => {
+    graphql.mockReturnValue({ data: null })
+    await createPages({ graphql } as any, {} as any, jest.fn())
+
+    expect(icsSpy).not.toHaveBeenCalled()
+    expect(writeFileMock).not.toHaveBeenCalled()
+  })
+
+  it('should create a calendar file for each season', async () => {
+    graphql.mockReturnValue({
+      data: {
+        allEvent: {
+          nodes: [
+            {
+              chapter: 'Toronto',
+              distance: 200,
+              event: 'brevet',
+              id: 1,
+              route: 'Waterfront East',
+              rwgpsUrl: 'https://ridewithgps.com/routes/20784084',
+              startLocation: 'Rouge GO',
+              date: 'August 7 2020 08:00 EDT',
+              season: '2020',
+              path: '/event/2021/waterfront-east-august-7-2020-08-00-edt'
+            },
+            {
+              chapter: 'Huron',
+              distance: 600,
+              eventType: 'brevet',
+              id: 2,
+              route: 'Brouse\'s Beach Browser',
+              rwgpsUrl: 'https://ridewithgps.com/routes/27865209',
+              startLocation: 'Goderich, Ontario',
+              date: 'August 14 2021 08:00 EDT',
+              season: '2021',
+              path: '/event/2021/brouses-beach-browser-august-14-2021-0800-edt'
+            }
+          ]
+        }
+      }
+    })
+    await createPages({ graphql } as any, {} as any, jest.fn())
+
+    expect(icsSpy).toHaveBeenNthCalledWith(2, [expect.objectContaining({
+      description: `Brouse's Beach Browser - 600km
+  Start location: Goderich, Ontario
+  Start time: Sat August 14 2021 08:00
+  Brevet time limit: 40 hours, 0 minutes
+  Chapter: Huron
+  Visit https://randoneurs.to/event//event/2021/brouses-beach-browser-august-14-2021-0800-edt for more information.\n`,
+      duration: { "hours": 40, "minutes": 0 },
+      location: "Goderich, Ontario",
+      start: [2021, 8, 14, 12, 0],
+      title: "Brouse's Beach Browser 600km brevet",
+      url: "https://randoneurs.to/event/2021/brouses-beach-browser-august-14-2021-0800-edt"
+    })])
+  })
+})
--- gatsby/source/randOnt/createPages.ts
+++ gatsby/source/randOnt/createPages.ts
@@ -1,14 +1,36 @@
 import { GatsbyNode } from "gatsby"
 import { existsSync, mkdirSync, writeFileSync } from 'fs'
-// import { EventCalendarFeedsQuery } from "src/gatsby.gql"
+import { EventCalendarFeedsQuery } from 'src/gatsby.gql'
+import { createEvents, EventAttributes } from 'ics'
+import { getDateTimeLong } from "src/utils"
 
 const eventPath = './public/event'
+const brevetTimeLimits = {
+  default: { hours: 6, minutes: 0 },
+  200: { hours: 13, minutes: 30 },
+  300: { hours: 20, minutes: 0 },
+  400: { hours: 27, minutes: 0 },
+  600: { hours: 40, minutes: 0 },
+  1000: { hours: 75, minutes: 0 },
+  1200: { hours: 90, minutes: 0 },
+  1400: { hours: 116, minutes: 40 },
+  2200: { hours: 220, minutes: 0 }
+}
 
-export const createPages: GatsbyNode['createPages'] = async ({ graphql, ...args }) => {
-  console.log('hello')
-  const { data } = await graphql(`
+const buildCalendarDescription = (event: EventCalendarFeedsQuery['allEvent']['nodes'][0]) =>
+(`${event.route} - ${event.distance}km
+  Start location: ${event.startLocation}
+  Start time: ${getDateTimeLong(new Date(event.date))}
+  Brevet time limit: ${brevetTimeLimits[event.distance].hours} hours, ${brevetTimeLimits[event.distance].minutes} minutes
+  Chapter: ${event.chapter}\n\
+  Visit https://randoneurs.to/event/${event.path} for more information.
+`)
+
+export const createPages: GatsbyNode['createPages'] = async ({ graphql }) => {
+  const seasons = new Map<string, EventAttributes[]>()
+  const { data } = await graphql<EventCalendarFeedsQuery>(`
     query EventCalendarFeeds {
-      allEvent {
+      allEvent(sort: {date: ASC}) {
         nodes {
           chapter
           distance
@@ -32,9 +54,18 @@ export const createPages: GatsbyNode['createPages'] = async ({ graphql, ...args 
     mkdirSync(eventPath)
   }
 
-  data.allEvent.nodes.map(event => {
-    const data = { ...event }
-    writeFileSync(`${eventPath}/${data.id}.json`, JSON.stringify(data))
+  data.allEvent.nodes.forEach((event) => {
+    const date = new Date(event.date)
+    seasons.set(event.season, [...(seasons.get(event.season) || []), {
+      start: [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes()],
+      title: `${event.route} ${event.distance}km ${event.eventType}`,
+      description: buildCalendarDescription(event),
+      location: event.startLocation,
+      url: `https://randoneurs.to${event.path}`,
+      duration: brevetTimeLimits[event.distance || 'default'],
+    }])
   })
-  console.log('Found something!')
+  seasons.forEach((events, season) =>
+    writeFileSync(`${eventPath}/${season}.ics`, createEvents(events).value)
+  )
 }
